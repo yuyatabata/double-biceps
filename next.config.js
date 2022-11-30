2fb7fe131bed4645d1a4203eec6768a106c41dca
yuyatabata <tabayu5@gmail.com>
2022-11-30 20:36:05 +0900

    NextAuth.jsの導入

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,13 @@
-import App from "next/app";
-App.getInitialProps = async () => ({ pageProps: {} });
-export default App;
+import { Session } from "next-auth";
+import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+
+const MyApp = ({ Component, pageProps }: AppProps<{ session: Session }>) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default MyApp;
