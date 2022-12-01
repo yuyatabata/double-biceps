8478ba07a8e2248e9d6a78cf9979694d6333e76f
yuyatabata <tabayu5@gmail.com>
2022-12-01 09:24:29 +0900

    add PrismaAdapter&change schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signIn("google")}>Sign in</button>
     </>
   );
 };
